b8041cb487371463924890cc5e0373951e166c0d
gabi brown <gbrown@lcs.on.ca>
2022-02-25 15:14:46 -0500

    added second function and encoder

--- QuotesApp/Views/ContentView.swift
+++ QuotesApp/Views/ContentView.swift
@@ -109,7 +109,39 @@ struct ContentView: View {
         currentQuoteAddedToFavourites = false
     }
     
-    
+    // Saves (persists) the data to local storage on the device
+    func persistQuotes() {
+        
+        // Get a URL that points to the saved JSON data containing our list of tasks
+        let filename = getDocumentsDirectory().appendingPathComponent(savedQuotesLabel)
+        
+        // Try to encode the data in our people array to JSON
+        do {
+            // Create an encoder
+            let encoder = JSONEncoder()
+            
+            // Ensure the JSON written to the file is human-readable
+            encoder.outputFormatting = .prettyPrinted
+            
+            // Encode the list of favourites we've collected
+            let data = try encoder.encode(favourites)
+            
+            // Actually write the JSON file to the documents directory
+            try data.write(to: filename, options: [.atomicWrite, .completeFileProtection])
+            
+            // See the data that was written
+            print("Saved data to documents directory successfully.")
+            print("===")
+            print(String(data: data, encoding: .utf8)!)
+            
+        } catch {
+            
+            print(error.localizedDescription)
+            print("Unable to write list of favourites to documents directory in app bundle on device.")
+            
+        }
+
+    }
 }
 
 
